5717544f29bb49fece72c0c93cdd12ca6b124a94
Kacper Wojcik <kwojcik@student.agh.edu.pl>
2020-11-12 19:04:08 +0100

    wersja z crontab 2

--- cron.sh
+++ cron.sh
@@ -3,13 +3,16 @@
 if [ "$users" = "" ]
 then
 users > tymczas2.txt
-email="balayoextreme7@gmail.com"
+email="adres emial"
 echo "Zalogowani uzytkownicy: $(cat tymczas2.txt)" | mutt -s "Zalogowani uzytkownicy" $email
 rm tymczas2.txt
 else
-email="balayoextreme7@gmail.com"
+email="adres email"
 echo "Brak zalogowanych użytkowników" $email
 fi
+
+# crontab:
+# */0 12 * * * /home/imirgrp/kwojcik/Laboratoria-informatyka/cron.sh 
 
 
 
